102f0d17a8c5988738f3c2bfcb1300861e3ccabe
Tim <timtran007@gmail.com>
2022-10-04 00:00:21 -0500

    commit tree 82d960d1423fde34271ba04306af80b439692f04

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+    let formData = {
+        name: userName,
+        email: userEmail,
+    }
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+   return fetch('http://localhost:3000/users', configurationObject)
+        .then(res => res.json())
+        .then(data => {
+            let id = data.id
+            document.body.append(id)
+        })
+        .catch(error =>{
+            document.body.append(error.message)
+        })
+}
+
